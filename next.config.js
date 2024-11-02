8a48f44735e964422a615bfaea33b319c6dd90dc
Pieter <pieter.bergwerff@sdbgroep.nl>
2024-11-02 14:17:27 +0100

    Fix

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/string-extract",
+  distDir: "out",
+};
+
+module.exports = nextConfig;
